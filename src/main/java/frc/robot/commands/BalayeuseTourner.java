7df51bb60c97fe5dc88f01657731d862a5925a98
Samuel desharnais <sdesharnais@csbf.qc.ca>
2022-02-28 18:55:31 -0500

    bn

--- src/main/java/frc/robot/commands/BalayeuseTourner.java
+++ src/main/java/frc/robot/commands/BalayeuseTourner.java
@@ -5,20 +5,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Balayeuse;
 
 public class BalayeuseTourner extends CommandBase {
+
+  private Balayeuse m_balayeuse;
+
   /** Creates a new BalayeuseTourner. */
-  public BalayeuseTourner() {
+  public BalayeuseTourner(Balayeuse balayeuse) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_balayeuse = balayeuse;
+    addRequirements(m_balayeuse);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_balayeuse.tourneshaft(0.5);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
