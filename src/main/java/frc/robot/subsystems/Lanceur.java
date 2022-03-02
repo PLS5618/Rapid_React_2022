9a9f92b0ec98c3bd9b707aeb7fda4170210685cf
Samuel desharnais <sdesharnais@csbf.qc.ca>
2022-03-02 18:36:27 -0500

    nfhn

--- src/main/java/frc/robot/subsystems/Lanceur.java
+++ src/main/java/frc/robot/subsystems/Lanceur.java
@@ -6,12 +6,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import static frc.robot.Constants.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lanceur extends SubsystemBase {
-  private TalonSRX m_Lancer1 = new TalonSRX(0);
-  private TalonSRX m_Lancer2 = new TalonSRX(0);
+  private TalonSRX m_Lancer1 = new TalonSRX(kLancer1);
+  private TalonSRX m_Lancer2 = new TalonSRX(kLancer2);
   /** Creates a new lanceur. */
   public void lancer(double vitesse ){
     m_Lancer1.set (ControlMode.PercentOutput, vitesse);
