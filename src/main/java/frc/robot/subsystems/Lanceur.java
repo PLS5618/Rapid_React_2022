80c7f63a63acf7cf4d4888d30b85e26d65b974c5
Samuel desharnais <sdesharnais@csbf.qc.ca>
2022-03-02 18:25:50 -0500

    uo78o

---
+++ src/main/java/frc/robot/subsystems/Lanceur.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lanceur extends SubsystemBase {
+  private TalonSRX m_Lancer1 = new TalonSRX(0);
+  private TalonSRX m_Lancer2 = new TalonSRX(0);
+  /** Creates a new lanceur. */
+  public void lancer(double vitesse ){
+    m_Lancer1.set (ControlMode.PercentOutput, vitesse);
+    m_Lancer2.set (ControlMode.PercentOutput, vitesse);
+     
+  }
+  public Lanceur() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
