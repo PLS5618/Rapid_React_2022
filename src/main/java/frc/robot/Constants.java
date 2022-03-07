6a5622532501e68a5642cb2bde71073d0d5c01fd
Samuel desharnais <sdesharnais@csbf.qc.ca>
2022-03-07 18:48:18 -0500

    ,.j,

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,10 +37,10 @@ public final class Constants {
     public static int kBalayeusePiston4 = 3;
     public static int kAccumulateurBallonAvD = 4;
     public static int kAccumulateurBallonArD = 5;
-    public static int kElevateurLimitSwitch1 = 6;
-    public static int kElevateurLimitSwitch2 = 7;
-    public static int kElevateurLimitSwitch3 = 8;
-    public static int kElevateurLimitSwitch4 = 19;
+    public static int kElevateurLimitSwitchhautd = 6;
+    public static int kElevateurLimitSwitchhautg = 7;
+    public static int kElevateurLimitSwitchbasd = 8;
+    public static int kElevateurLimitSwitchbasg = 19;
     public static int kAccumulateurBallonAvG = 10;
     public static int kAccumulateurBallonArG = 11;
     
