becd111c059c9d99b60765d2877e71f7d9a194ed
Samuel desharnais <sdesharnais@csbf.qc.ca>
2022-03-09 17:58:29 -0500

    vnmn

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,7 +70,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new Joystick 
+    
   } 
 
   /**
